178adad33894031df43510b7b070bc1ccd094e41
slim <noreply@puppet.com>
2024-08-03 09:40:16 -0400

    some shit

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,4 +1,13 @@
 # The base profile should include component modules that will be on all nodes
+# site-modules/profile/manifests/base.pp
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profile::base
 class profile::base {
-
+  class { 'motd':
+    content => "Hello! TPL are in ${trusted['extensions']['hostname']}.\n"
+  }
 }
