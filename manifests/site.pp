5f7820c3533b47ea129e3ee069be30523eb21660
slim <dennisjohnson104@gmail.com>
2024-08-04 15:55:13 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -26,7 +26,6 @@ File { backup => false }
 # For more on node definitions, see: https://puppet.com/docs/puppet/latest/lang_node_definitions.html
 node default {
   include "role::webserver"
-  include motd
 }
 
 
