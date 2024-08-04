7ecfb9bef515267c168eed41187be798370869d9
slim <dennisjohnson104@gmail.com>
2024-08-04 19:13:08 +0000

    Update base.pp

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -8,7 +8,7 @@
 #   include profile::base
 class profile::base {
   class { 'motd':
-      content => "Hello! You are in the  ${facts['environment'] environment}\n"
+      content => "Hello! You are in the ${facts['environment']} environment \n"
   }
 }
 
