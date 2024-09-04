bfc7e3a80faf163be340893ae079317ca034a6b6
slim <noreply@puppet.com>
2024-09-04 07:44:57 -0400

    updated webserver module with date

--- site-modules/role/manifests/webserver.pp
+++ site-modules/role/manifests/webserver.pp
@@ -1,5 +1,5 @@
 # This role would be made of all the profiles that need to be included to make a webserver work
-# All roles should include the base profiles
+# All roles should include the base profiles 3/4/2024
 class role::webserver {
   include:: profile::base
   include:: profile::iis
