cf2e202f5392d34a1091a4fee8292eb56346204b
slim <dennisjohnson104@gmail.com>
2024-09-05 23:22:55 +0000

    Update base.pp

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -8,7 +8,7 @@
 #   include profile::base
 class profile::base {
   class { 'motd':
-      content => "Hello! You are in the ${facts['environment']} environment of TPL \n"
+      message => "HELLO"
   }
 }
 
