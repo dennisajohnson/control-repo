5a03801bceb9a1f6f9bcceafcf5fa166b894cc35
slim <dennisjohnson104@gmail.com>
2024-09-17 20:05:25 +0000

    Update sa.pp

--- site-modules/profile/manifests/sa.pp
+++ site-modules/profile/manifests/sa.pp
@@ -1,6 +1,6 @@
 class profile::sa {
   exec { 'add SAs to RDP and Admin':
   command   => 'Add-localGroupMember -group "Remote Desktop Users" -member "tpl\ServerAdmins"';'Add-localGroupMember -group "Administrators" -member "tpl\ServerAdmins"',
-  provider  => powershell,
+  provider  => powershell
     }
 }
