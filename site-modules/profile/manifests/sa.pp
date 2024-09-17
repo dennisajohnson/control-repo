030f37244c5ba40455459fe709a2e4f4afeb350a
slim <dennisjohnson104@gmail.com>
2024-09-17 19:19:56 +0000

    Update sa.pp

--- site-modules/profile/manifests/sa.pp
+++ site-modules/profile/manifests/sa.pp
@@ -1,5 +1,5 @@
-class example_class {
-  ...exec { 'rename-guest':
+class sa {
+  exec { 'rename-guest':
   command   => 'get-date | out-file -filepath c:\time.txt',
   provider  => powershell,
     }
