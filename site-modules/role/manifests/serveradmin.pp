96307031009034f79e0545c5fec08a599fd9d27e
slim <noreply@puppet.com>
2024-09-24 10:29:54 -0400

    Manifiest to add the GroupName to RDP and Admin groups.

--- site-modules/role/manifests/serveradmin.pp
+++ site-modules/role/manifests/serveradmin.pp
@@ -1,7 +1,8 @@
-##DJ Change domain and add the group that you want to add to the RDP and Admin groups
+##DJ Change domain and group. 
+##Adds the group defined to RDP and Admin groups
 class role::serveradmin {
     exec { 'add SAs to RDP and Admin':
-  command   => 'Add-localGroupMember -group "Remote Desktop Users" -member "domain\ServerAdmins" ; Add-localGroupMember -group "Administrators" -member "domain\ServerAdmins"',
+  command   => 'Add-localGroupMember -group "Remote Desktop Users" -member "domain\GroupName" ; Add-localGroupMember -group "Administrators" -member "domain\GroupName"',
   provider  => powershell
     }
 }
