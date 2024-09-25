7fac833d2cec9013d46eba76627365543603cbfe
slim <noreply@puppet.com>
2024-09-24 20:10:32 -0400

    changed motd to development branch

--- site-modules/profile/manifests/motd.pp
+++ site-modules/profile/manifests/motd.pp
@@ -1,6 +1,6 @@
 class profile::motd {
       class { 'motd':
-      content => "Hello! You are pulling from the production repo \n"
+      content => "Hello! You are pulling from the development repo \n"
   }
 
 }
