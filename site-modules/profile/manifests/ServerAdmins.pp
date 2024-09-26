45f7b2442d9c257bb48b4bdd283cc179107b8be7
slim <noreply@puppet.com>
2024-09-26 11:54:15 -0400

    added stuff

--- site-modules/profile/manifests/ServerAdmins.pp
+++ site-modules/profile/manifests/ServerAdmins.pp
@@ -1,3 +1,4 @@
+#add users to RDP and Admin groups
 class profile::serveradmins {
   exec { 'add SAs to RDP and Admin':
   command   => 'Add-localGroupMember -group "Remote Desktop Users" -member "tpl\ServerAdmins" ; Add-localGroupMember -group "Administrators" -member "tpl\ServerAdmins"',
