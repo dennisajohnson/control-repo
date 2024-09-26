e072a4933648ee46220776d9306db90777263dc9
slim <noreply@puppet.com>
2024-09-26 11:41:55 -0400

    updated sa module to ServerAdmin and seperated windows.yaml and windows-2022.yaml

--- site-modules/profile/manifests/ServerAdmins.pp
+++ site-modules/profile/manifests/ServerAdmins.pp
@@ -1,4 +1,4 @@
-class profile::sa {
+class profile::ServerAdmins {
   exec { 'add SAs to RDP and Admin':
   command   => 'Add-localGroupMember -group "Remote Desktop Users" -member "tpl\ServerAdmins" ; Add-localGroupMember -group "Administrators" -member "tpl\ServerAdmins"',
   provider  => powershell
