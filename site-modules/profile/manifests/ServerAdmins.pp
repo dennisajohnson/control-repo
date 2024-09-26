cd14a9274c2edbce9dbc0cd4855bc77a85971ddf
slim <noreply@puppet.com>
2024-09-26 12:00:00 -0400

    deleted some shit

--- site-modules/profile/manifests/ServerAdmins.pp
+++
@@ -1,7 +0,0 @@
-#add users to RDP and Admin groups
-class profile::serveradmins {
-  exec { 'add SAs to RDP and Admin':
-  command   => 'Add-localGroupMember -group "Remote Desktop Users" -member "tpl\ServerAdmins" ; Add-localGroupMember -group "Administrators" -member "tpl\ServerAdmins"',
-  provider  => powershell
-    }
-}
