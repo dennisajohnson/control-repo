78feaf66951b1c571a9ee0dd5c64273ab48c4786
slim <noreply@puppet.com>
2024-09-26 11:52:17 -0400

    renamed ServerAdmins to serveradmins

--- site-modules/profile/manifests/ServerAdmins.pp
+++ site-modules/profile/manifests/ServerAdmins.pp
@@ -1,4 +1,4 @@
-class profile::ServerAdmins {
+class profile::serveradmins {
   exec { 'add SAs to RDP and Admin':
   command   => 'Add-localGroupMember -group "Remote Desktop Users" -member "tpl\ServerAdmins" ; Add-localGroupMember -group "Administrators" -member "tpl\ServerAdmins"',
   provider  => powershell
