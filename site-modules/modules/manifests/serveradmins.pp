e9d89a8db83a0e02ab8102c1eb609a0f52f65f07
slim <dennisjohnson104@gmail.com>
2024-10-06 15:10:38 +0000

    Add new file

---
+++ site-modules/modules/manifests/serveradmins.pp
@@ -0,0 +1,6 @@
+class module::serveradmin {
+  exec { 'add SAs to RDP and Admin':
+  command   => 'Add-localGroupMember -group "Remote Desktop Users" -member "tpl\ServerAdmins" ; Add-localGroupMember -group "Administrators" -member "tpl\ServerAdmins"',
+  provider  => powershell
+    }
+}
