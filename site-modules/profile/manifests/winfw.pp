b20b4422346b8ab9a8e9d2e92cfb4124e53e0b4f
slim <dennisjohnson104@gmail.com>
2024-10-06 15:11:44 +0000

    Delete winfw.pp

--- site-modules/profile/manifests/winfw.pp
+++
@@ -1,9 +0,0 @@
-class profile::disable_firewall {
-
-  exec { 'Disable-WindowsFirewall':
-    command => 'powershell.exe -Command "Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False"',
-    provider => powershell,
-    unless  => 'powershell.exe -Command "(Get-NetFirewallProfile -Profile Domain).Enabled -eq $False -and (Get-NetFirewallProfile -Profile Public).Enabled -eq $False -and (Get-NetFirewallProfile -Profile Private).Enabled -eq $False"',
-  }
-
-}
