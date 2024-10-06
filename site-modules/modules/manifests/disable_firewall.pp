c03642e802b2e9892b03a3c97a34280882b40579
slim <dennisjohnson104@gmail.com>
2024-10-06 14:31:34 +0000

    Add new file

---
+++ site-modules/modules/manifests/disable_firewall.pp
@@ -0,0 +1,9 @@
+class modules::disable_firewall {
+
+  exec { 'Disable-WindowsFirewall':
+    command => 'powershell.exe -Command "Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False"',
+    provider => powershell,
+    unless  => 'powershell.exe -Command "(Get-NetFirewallProfile -Profile Domain).Enabled -eq $False -and (Get-NetFirewallProfile -Profile Public).Enabled -eq $False -and (Get-NetFirewallProfile -Profile Private).Enabled -eq $False"',
+  }
+
+}
