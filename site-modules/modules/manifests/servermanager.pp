bfc733a967c477d9a5c5b205e903c3901fc1781e
slim <dennisjohnson104@gmail.com>
2024-10-09 22:04:02 +0000

    Update servermanager.pp

--- site-modules/modules/manifests/servermanager.pp
+++ site-modules/modules/manifests/servermanager.pp
@@ -1,7 +1,7 @@
 class modules::servermanager {
-  exec { "disable_scheduled_":
-    command => "schtasks /Change /TN 'ServerManager' /DISABLE",
-    onlyif  => "schtasks /Query /TN 'ServerManager' | findstr 'ServerManager'",
-    provider => powershell,
-  }
+exec { 'Set Registry Key':
+  command   => 'powershell.exe -Command "New-ItemProperty -Path \"HKLM:\\Software\\MyCompany\" -Name \"MyKey\" -Value \"MyValue\" -PropertyType String"',
+  provider  => powershell,
+  unless    => 'powershell.exe -Command "Get-ItemProperty -Path \"HKLM:\\Software\\MyCompany\" -Name \"MyKey\"" | Select-Object -ExpandProperty MyKey -ErrorAction SilentlyContinue',
+}
 }
