96bd6b4fc2e1d52a12081bf503601a5c71073d48
slim <dennisjohnson104@gmail.com>
2024-10-09 16:19:36 +0000

    Update servermanager.pp

--- site-modules/modules/manifests/servermanager.pp
+++ site-modules/modules/manifests/servermanager.pp
@@ -2,7 +2,7 @@ class modules::servermanager (
   $task_name = 'ServerManager',
 ) {
   exec { 'Check and Disable Server Manager Scheduled Task':
-    command => "powershell.exe -Command \"if (Get-ScheduledTask -TaskName '${task_name}') { Disable-ScheduledTask -TaskName '${task_name}'; Write-Output '${task_name} has been disabled.' } else { Write-Output '${task_name} does not exist.' }\"",
+    command => "powershell.exe -Command \"if (Get-ScheduledTask -TaskName '${task_name}') { Disable-ScheduledTask -TaskName '${task_name}'; exit 0 } else { exit 1 }\"",
     provider => powershell,
     onlyif  => "powershell.exe -Command \"if (Get-ScheduledTask -TaskName '${task_name}') { exit 0 } else { exit 1 }\"",
   }
