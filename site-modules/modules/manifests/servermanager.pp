4e84e2d96f8147616d137ab783fd0846e8973dfb
slim <dennisjohnson104@gmail.com>
2024-10-09 22:47:24 +0000

    Update servermanager.pp

--- site-modules/modules/manifests/servermanager.pp
+++ site-modules/modules/manifests/servermanager.pp
@@ -1,5 +1,5 @@
 class modules::servermanager (
-  String $task_name servermanager,
+  String $task_name = servermanager,
 ) {
   exec { 'Check and Disable Scheduled Task':
     command => "powershell.exe -Command \"\$task = Get-ScheduledTask -TaskName '${task_name}'; if (\$task.State -eq 'Ready') { Disable-ScheduledTask -TaskName '${task_name}'; Write-Output '${task_name} has been disabled.' } elseif (\$task.State -eq 'Disabled') { Write-Output '${task_name} is already disabled.' } else { Write-Output '${task_name} is in state: ' + \$task.State }\"",
