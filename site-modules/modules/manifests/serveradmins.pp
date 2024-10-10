f605809ee64700f37e43f08e1c6ced7048a8da0c
slim <dennisjohnson104@gmail.com>
2024-10-10 16:47:43 +0000

    Update file serveradmins.pp

--- site-modules/modules/manifests/serveradmins.pp
+++ site-modules/modules/manifests/serveradmins.pp
@@ -2,13 +2,13 @@ class modules::serveradmins {
   exec { 'add SAs to RDP':
   command   => 'Add-localGroupMember -group "Remote Desktop Users" -member "tpl\ServerAdmins"',
   unless =>  '$getrdpgroup = Get-LocalGroupMember -Group "Remote Desktop Users"
-      if ($getrdpgroup.name -ne "tpl\serveradmins" -and ) {exit 1} else {exit 0}',
+      if ($getrdpgroup.name -ne "tpl\serveradmins") {exit 1} else {exit 0}',
   provider  => powershell
     }
   exec { 'add SAs to Admin':
   command   => 'Add-localGroupMember -group "Administrators" -member "tpl\ServerAdmins"',
   unless =>  '$getadmingroup = Get-LocalGroupMember -Group "Administrators"
-      if ($getadmingroup.name -ne "tpl\serveradmins" -and ) {exit 1} else {exit 0}',
+      if ($getadmingroup.name -ne "tpl\serveradmins") {exit 1} else {exit 0}',
   provider  => powershell
     }
 }
