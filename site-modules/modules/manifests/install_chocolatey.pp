711484d6ff8de28bb8df5d12a2e55b1f3f695bd4
slim <dennisjohnson104@gmail.com>
2024-10-10 21:44:46 +0000

    Add new file

---
+++ site-modules/modules/manifests/install_chocolatey.pp
@@ -0,0 +1,6 @@
+class install_chocolatey {
+  exec { 'Install Chocolatey':
+    command => "powershell.exe -Command \"Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))\"",
+    provider => powershell,
+  }
+}
