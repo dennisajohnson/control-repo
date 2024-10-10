8f3a84a0958341345d621c1d04192b105855726b
slim <dennisjohnson104@gmail.com>
2024-10-10 15:36:15 +0000

    Update file servermanager.pp

--- site-modules/modules/manifests/servermanager.pp
+++ site-modules/modules/manifests/servermanager.pp
@@ -2,7 +2,7 @@ class modules::servermanager{
 exec { 'Start wuauserv':
   command  => 'Start-Service -Name "wuauserv"',
   unless => '$servicestatus = Get-Service -Name "wuauserv";
-             if ($servicestatus.Status -eq "Running") { Exit 1 } else { Exit 0 }',
+             if ($servicestatus.Status -eq "Stopped") { Exit 1 } else { Exit 0 }',
   provider  => powershell,
 }
 }
