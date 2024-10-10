5358cba93de02fcf74ce370aa89f84080e6a9029
slim <dennisjohnson104@gmail.com>
2024-10-10 16:02:31 +0000

    Update file servermanager.pp

--- site-modules/modules/manifests/servermanager.pp
+++ site-modules/modules/manifests/servermanager.pp
@@ -1,7 +1,7 @@
 class modules::servermanager{
 exec { 'Disable ServerManger at Login':
-  command  => 'disable-ScheduledTask -TaskPath '\Microsoft\Windows\Server Manager\' -TaskName ServerManager',
-  unless => '$ststatus = Get-ScheduledTask -TaskPath '\Microsoft\Windows\Server Manager\' -TaskName ServerManager;
+  command  => 'disable-ScheduledTask -TaskPath "\Microsoft\Windows\Server Manager\" -TaskName ServerManager',
+  unless => '$ststatus = Get-ScheduledTask -TaskPath "\Microsoft\Windows\Server Manager\" -TaskName ServerManager;
              if ($ststatus.state -eq "disabled") { Exit 1 } else { Exit 0 }',
   provider  => powershell,
 }
