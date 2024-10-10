82c8f9353d40ee72578d2684c47d932783df50ac
slim <dennisjohnson104@gmail.com>
2024-10-10 15:48:29 +0000

    Update file servermanager.pp

--- site-modules/modules/manifests/servermanager.pp
+++ site-modules/modules/manifests/servermanager.pp
@@ -1,5 +1,5 @@
 class modules::servermanager{
-exec { 'Start wuauserv':
+exec { 'Disable ServerManger at Login':
   command  => 'disable-ScheduledTask -TaskPath '\Microsoft\Windows\Server Manager\' -TaskName ServerManager',
   unless => '$ststatus = Get-ScheduledTask -TaskPath '\Microsoft\Windows\Server Manager\' -TaskName ServerManager;
              if ($ststatus.state -eq "disabled") { Exit 1 } else { Exit 0 }',
