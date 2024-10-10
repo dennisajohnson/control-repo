39bdb55a0c924ec93ddf59b55c1d934dba781fba
slim <dennisjohnson104@gmail.com>
2024-10-10 15:03:39 +0000

    Update file servermanager.pp

--- site-modules/modules/manifests/servermanager.pp
+++ site-modules/modules/manifests/servermanager.pp
@@ -1,6 +1,6 @@
 class modules::servermanager {
   exec { 'Check Windows Update Service Status':
-    command => "powershell.exe -Command (Get-Service -Name wuauserv).Status; if ((Get-Service -Name wuauserv).Status -eq 'Running') { Write-Output 'Windows Update service is running.' } else { Write-Output 'Windows Update service is not running.'; exit 1 } \"",
+    command => "powershell.exe -Command (Get-Service -Name wuauserv).Status; if ((Get-Service -Name wuauserv).Status -ne 'Running') { start-service -name wuauserv} \"",
     provider => powershell,
     onlyif  => "powershell.exe -Command (Get-Service -Name wuauserv).Status; if ((Get-Service -Name wuauserv).Status -eq 'Running') { exit 0 } else { exit 1 }\"",
   }
