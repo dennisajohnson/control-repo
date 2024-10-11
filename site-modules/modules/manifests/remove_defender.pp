9204b01d574ca8c39338ea546c99db37084d9ebb
slim <dennisjohnson104@gmail.com>
2024-10-11 18:28:48 +0000

    Update file remove_defender.pp

--- site-modules/modules/manifests/remove_defender.pp
+++ site-modules/modules/manifests/remove_defender.pp
@@ -1,8 +1,8 @@
 class modules::remove_defender{
 exec { 'Disable ServerManger at Login':
   command  => 'Uninstall-WindowsFeature -Name Windows-Defender'
-  unless => '$ststatus =  get-WindowsFeature -Name Windows-Defender
+  unless   => '$ststatus =  get-WindowsFeature -Name Windows-Defender
              if ($ststatus.installstate -eq "Installed") { Exit 1 } else { Exit 0 }',
-  provider  => powershell,
+  provider => powershell,
 }
 }
