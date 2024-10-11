a926bae16e4a6c210e2852d45d451673093726f7
slim <dennisjohnson104@gmail.com>
2024-10-11 18:36:46 +0000

    Update remove_defender.pp

--- site-modules/modules/manifests/remove_defender.pp
+++ site-modules/modules/manifests/remove_defender.pp
@@ -1,25 +1,20 @@
 class modules::remove_defender{
 exec { 'remove defender':
   command  => 'Uninstall-WindowsFeature -Name Windows-Defender'
-  unless   => '$ststatus =  get-WindowsFeature -Name Windows-Defender
-             if ($ststatus.installstate -eq "Installed") { Exit 1 } else { Exit 0 }',
+  unless   => '$dfstatus =  get-WindowsFeature -Name Windows-Defender
+             if ($dfstatus.installstate -eq "Installed") { Exit 1 } else { Exit 0 }',
   provider => powershell
 }
 }
 
 /*
-class modules::serveradmins {
-  exec { 'add SAs to RDP':
-  command   => 'Add-localGroupMember -group "Remote Desktop Users" -member "tpl\ServerAdmins"',
-  unless =>  '$getrdpgroup = Get-LocalGroupMember -Group "Remote Desktop Users"
-      if ($getrdpgroup.name -ne "tpl\serveradmins") {exit 1} else {exit 0}',
-  provider  => powershell
-    }
-  exec { 'add SAs to Admin':
-  command   => 'Add-localGroupMember -group "Administrators" -member "tpl\ServerAdmins"',
-  unless =>  '$getadmingroup = Get-LocalGroupMember -Group "Administrators"
-      if ($getadmingroup.name -eq "tpl\serveradmins") {exit 1} else {exit 0}',
-  provider  => powershell
-    }
+class modules::servermanager{
+exec { 'Disable ServerManger at Login':
+  command  => 'disable-ScheduledTask -TaskPath "\Microsoft\Windows\Server Manager\" -TaskName ServerManager',
+  unless => '$ststatus = Get-ScheduledTask -TaskPath "\Microsoft\Windows\Server Manager\" -TaskName ServerManager;
+             if ($ststatus.state -eq "Ready") { Exit 1 } else { Exit 0 }',
+  provider  => powershell,
 }
+}
+
 */
