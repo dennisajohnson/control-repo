046c372804ab88e817842b1591a24902e7409732
slim <dennisjohnson104@gmail.com>
2024-10-11 18:17:57 +0000

    Update remove_defender.pp

--- site-modules/modules/manifests/remove_defender.pp
+++ site-modules/modules/manifests/remove_defender.pp
@@ -1,16 +1,8 @@
-class modules::remove_defender {
-
-  # Check if Windows Defender is installed
-  exec { 'Check Windows Defender':
-    command => "powershell.exe -Command \"if (Get-WmiObject -Namespace 'Root\\Microsoft\\Windows\\Defender' -Class 'MSFT_MpComputerStatus') { Write-Output 'Windows Defender is installed.' } else { Write-Output 'Windows Defender is not installed.'; exit 1 }\"",
-    provider => powershell,
-  }
-
-  # Uninstall Windows Defender if installed
-  exec { 'Uninstall Windows Defender':
-    command => "powershell.exe -Command \"Get-AppxPackage -AllUsers *WindowsDefender* | Remove-AppxPackage\"",
-    provider => powershell,
-    onlyif  => "powershell.exe -Command \"if (Get-WmiObject -Namespace 'Root\\Microsoft\\Windows\\Defender' -Class 'MSFT_MpComputerStatus') { exit 0 } else { exit 1 }\"",
-  }
-
+class modules::remove_defender{
+exec { 'Disable ServerManger at Login':
+  command  => 'Uninstall-WindowsFeature -Name Windows-Defender'
+  unless => '$ststatus =  get-WindowsFeature -Name Windows-Defender;
+             if ($ststatus.installstate -eq "Installed") { Exit 1 } else { Exit 0 }',
+  provider  => powershell,
+}
 }
